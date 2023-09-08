0a0229b1253cb5aabf632ed982cdc44a2e4ec607
Azrubael <i0638464000@gmail.com>
2023-09-08 20:23:20 +0300

    Step #10. Bash scripts first commit. Simples samples on CentOS7.

---
+++ 68-bash-scripts/scripts/firstscript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Welcome to bash script."
+echo
+echo "##############################################"
+echo "The uptime of the system is: "
+uptime
+echo 
+echo "##############################################"
+echo "Memory utilization: "
+free -m
+echo 
+echo "##############################################"
+echo "Disk utilization: "
+df -h
