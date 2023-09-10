fed79b4e7f2d714f846b8d5aab60c622989a1a86
Azrubael <i0638464000@gmail.com>
2023-09-10 19:47:45 +0300

    Step #17. New Bash scripts for if/then/elif/else/fi and  loop.

---
+++ 68-bash-scripts/scripts/o11-monit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "#####################################################"
+ls /var/run/httpd/httpd.pid &> /dev/null
+
+if [ $? -eq 0 ]
+then
+   echo "Httpd process is running."
+else
+   echo "Httpd process is NOT Running."
+   echo "Starting the process"
+   systemctl start httpd 
+   if [ $? -eq 0 ]
+   then
+      echo "Process started successfully."
+   else
+      echo "Process Starting Failed, contact the admin."
+   fi
+fi
+date 
+echo "#####################################################"
+echo 
