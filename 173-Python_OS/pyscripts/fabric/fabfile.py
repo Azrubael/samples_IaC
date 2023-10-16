7d0c7b32650844259a0ad1388e350f9968bdab1f
Azrubael <i0638464000@gmail.com>
2023-10-16 18:48:19 +0300

    Step #4r98. '173-Python_OS/. Added 'fabric.py' script and a worklog  with setup of remote access via ssh with ssh-key.

---
+++ 173-Python_OS/pyscripts/fabric/fabfile.py
@@ -0,0 +1,21 @@
+from fabric.api import *
+
+def greeting(msg):
+    print("Good {}".format(msg))
+
+def system_info():
+    """The function to run on a local machine"""
+    print("Disk space -------------------------------")
+    local("df -h")
+    print("RAM size ---------------------------------")
+    local("free -m")
+    print("System uptime ----------------------------")
+    local("uptime")
+
+def remote_exec():
+    """The function to run on a remote machine."""
+    print("Get System Info --------------------------")
+    run("hostname")
+    run("uptime")
+    run("ps -eF")
+    run("du -ht 1M /home -d3")
