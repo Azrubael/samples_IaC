e985b6caa410ea0dbafcb2cad62dad55e863e1ab
Azrubael <i0638464000@gmail.com>
2023-10-21 20:23:52 +0300

    Step #54. '177-Ansible/' Exercise 8 updates.

--- 177-Ansible/scp.sh
+++ 177-Ansible/scp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for (( i=1; i<9; i++))
 do
-    scp -i "~/.aws/230724-ec2-t2micro.pem" /mnt/SSDATA/CODE/DevOpsCompl20/samples_IaC/177-Ansible/exercise${i}/* ubuntu@52.201.241.15:/home/ubuntu/vprofile/exercise${i}/
+    scp -r -i "~/.aws/230724-ec2-t2micro.pem" /mnt/SSDATA/CODE/DevOpsCompl20/samples_IaC/177-Ansible/exercise${i}/* ubuntu@52.201.241.15:/home/ubuntu/vprofile/exercise${i}/
 done
